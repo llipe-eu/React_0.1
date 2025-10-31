5bd2a88b4041df01d634c351ca7bef3de86c4bff
llipe-eu <llipegleu@gmail.com>
2025-10-30 22:28:14 -0300

    Create App component with MeuComponente

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MeuComponente from './components/MeuComponente.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Atividade React</h1>
+      <MeuComponente /> 
+    </div>
+  );
+}
+
+export default App;
